88c707dd5498ec5844a8c5252f22941806a77152
JASON PATIGAYON <jasonpatigayon25@gmail.com>
2023-06-25 21:31:33 +0800

    Project-ECOmercado

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Dropdown } from 'react-bootstrap';
 import Footer from './Footer';
 
 const Cart = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectAll, setSelectAll] = useState(false);
+  const [voucherCode, setVoucherCode] = useState('');
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [discount, setDiscount] = useState(0);
 
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log("Searching for:", searchQuery);
+    console.log('Searching for:', searchQuery);
   };
-
 
   const handleMouseEnter = (index) => {
     setHoveredIndex(index);
@@ -20,6 +23,63 @@ const Cart = () => {
 
   const handleMouseLeave = () => {
     setHoveredIndex(null);
+  };
+
+  const handleCheckboxChange = (event, item) => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      setSelectedItems((prevItems) => [...prevItems, item]);
+    } else {
+      setSelectedItems((prevItems) => prevItems.filter((selectedItem) => selectedItem.id !== item.id));
+    }
+    event.target.disabled = true;
+  };
+
+  const handleSelectAllChange = (event) => {
+    const isChecked = event.target.checked;
+    setSelectAll(isChecked);
+    if (isChecked) {
+      setSelectedItems(items);
+    } else {
+      setSelectedItems([]);
+    }
+  };
+
+  const handleVoucherCodeChange = (event) => {
+    setVoucherCode(event.target.value);
+    // Apply voucher code logic here to calculate the discount
+    // and update the `discount` state accordingly
+    setDiscount("");
+  };
+
+
+  const items = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 'Php 100.00',
+      quantity: 1,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 'Php 75.50',
+      quantity: 2,
+    },
+  ];
+
+  const calculateAmount = () => {
+    if (selectedItems.length === 0) {
+      return 'Php 0.00';
+    }
+
+    let totalAmount = 0;
+    selectedItems.forEach((item) => {
+      const price = parseFloat(item.price.substring(4)); // Extract the numerical value from price string
+      totalAmount += price * item.quantity;
+    });
+
+    return `Php ${totalAmount.toFixed(2)}`;
   };
 
   return (
@@ -31,7 +91,7 @@ const Cart = () => {
           height="60"
           className="d-inline-block align-top"
           alt="Logo"
-          style={{ marginLeft: "50px" }}
+          style={{ marginLeft: '50px' }}
         />
         <div className="container">
           <form className="d-flex justify-content-center" onSubmit={handleSearch}>
@@ -41,33 +101,33 @@ const Cart = () => {
               placeholder="Search Products"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              style={{ width: "600px", borderColor: "#05652D" }}
+              style={{ width: '600px', borderColor: '#05652D' }}
             />
-            <button className="btn" type="submit" style={{ borderColor: "#05652D" }}>
+            <button className="btn" type="submit" style={{ borderColor: '#05652D' }}>
               <img
-                src={process.env.PUBLIC_URL + "/search-icon.png"}
+                src={process.env.PUBLIC_URL + '/search-icon.png'}
                 alt="Search"
                 className="nav-icon"
                 style={{
-                  transform: hoveredIndex === 4 ? "scale(1.1)" : "scale(1)",
+                  transform: hoveredIndex === 4 ? 'scale(1.1)' : 'scale(1)',
                 }}
                 onMouseEnter={() => handleMouseEnter(4)}
                 onMouseLeave={handleMouseLeave}
               />
             </button>
           </form>
-          <button className="btn" type="submit" style={{ borderColor: "transparent" }}>
-          <Link to="/cart">
-            <img
-              src={process.env.PUBLIC_URL + "/shopping-cart.png"}
-              alt="Cart"
-              className="nav-icon"
-              style={{
-                transform: hoveredIndex === 3 ? "scale(1.1)" : "scale(1)",
-              }}
-              onMouseEnter={() => handleMouseEnter(3)}
-              onMouseLeave={handleMouseLeave}
-            />
+          <button className="btn" type="submit" style={{ borderColor: 'transparent' }}>
+            <Link to="/cart">
+              <img
+                src={process.env.PUBLIC_URL + '/shopping-cart.png'}
+                alt="Cart"
+                className="nav-icon"
+                style={{
+                  transform: hoveredIndex === 3 ? 'scale(1.1)' : 'scale(1)',
+                }}
+                onMouseEnter={() => handleMouseEnter(3)}
+                onMouseLeave={handleMouseLeave}
+              />
             </Link>
           </button>
           <div className="d-flex justify-content-end align-items-center w-100">
@@ -76,11 +136,11 @@ const Cart = () => {
                 <Link className="nav-link" to="/home">
                   <div className="d-flex flex-column align-items-center">
                     <img
-                      src={process.env.PUBLIC_URL + "/home.png"}
+                      src={process.env.PUBLIC_URL + '/home.png'}
                       alt="Home"
                       className="nav-icon"
                       style={{
-                        transform: hoveredIndex === 0 ? "scale(1.1)" : "scale(1)",
+                        transform: hoveredIndex === 0 ? 'scale(1.1)' : 'scale(1)',
                       }}
                       onMouseEnter={() => handleMouseEnter(0)}
                       onMouseLeave={handleMouseLeave}
@@ -93,11 +153,11 @@ const Cart = () => {
                 <Link className="nav-link" to="/home">
                   <div className="d-flex flex-column align-items-center">
                     <img
-                      src={process.env.PUBLIC_URL + "/notification.png"}
+                      src={process.env.PUBLIC_URL + '/notification.png'}
                       alt="notif"
                       className="nav-icon"
                       style={{
-                        transform: hoveredIndex === 1 ? "scale(1.1)" : "scale(1)",
+                        transform: hoveredIndex === 1 ? 'scale(1.1)' : 'scale(1)',
                       }}
                       onMouseEnter={() => handleMouseEnter(1)}
                       onMouseLeave={handleMouseLeave}
@@ -106,82 +166,129 @@ const Cart = () => {
                   </div>
                 </Link>
               </li>
-            <li className="nav-item dropdown">
-              <Dropdown>
-                <Dropdown.Toggle
-                  className="nav-link"
-                  style={{
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer',
-                  }}
-                >
-                  <div className="d-flex flex-column align-items-center">
-                    <img
-                      src={process.env.PUBLIC_URL + '/settings.png'}
-                      alt="Option"
-                      className="nav-icon"
-                      style={{
-                        transform: hoveredIndex === 2 ? 'scale(1.1)' : 'scale(1)',
-                      }}
-                      onMouseEnter={() => handleMouseEnter(2)}
-                      onMouseLeave={handleMouseLeave}
-                    />
-                  </div>
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="/change-account">Change Account</Dropdown.Item>
-                  <Dropdown.Item href="/change-password">Change Password</Dropdown.Item>
-                  <Dropdown.Item href="/language">Language</Dropdown.Item>
-                  <Dropdown.Item href="/help">Help</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="/">Logout</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </li>
+              <li className="nav-item dropdown">
+                <Dropdown>
+                  <Dropdown.Toggle
+                    className="nav-link"
+                    style={{
+                      background: 'none',
+                      border: 'none',
+                      cursor: 'pointer',
+                    }}
+                  >
+                    <div className="d-flex flex-column align-items-center">
+                      <img
+                        src={process.env.PUBLIC_URL + '/settings.png'}
+                        alt="Option"
+                        className="nav-icon"
+                        style={{
+                          transform: hoveredIndex === 2 ? 'scale(1.1)' : 'scale(1)',
+                        }}
+                        onMouseEnter={() => handleMouseEnter(2)}
+                        onMouseLeave={handleMouseLeave}
+                      />
+                    </div>
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="/change-account">Change Account</Dropdown.Item>
+                    <Dropdown.Item href="/change-password">Change Password</Dropdown.Item>
+                    <Dropdown.Item href="/language">Language</Dropdown.Item>
+                    <Dropdown.Item href="/help">Help</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="/">Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
 
       <div className="container mt-5">
-        <h2 className="text-center mb-4"  style={{ marginTop: "100px" }}>Shopping Cart</h2>
+        <h2 className="text-center mb-4" style={{ marginTop: '100px' }}>
+          Shopping Cart
+        </h2>
 
-        <Card className="mb-3">
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <Card.Img src="product1.jpg" alt="Product 1" />
-            </div>
-            <div className="col-md-8">
-              <Card.Body>
-                <Card.Title>Product 1</Card.Title>
-                <Card.Text>Price: Php 100.00</Card.Text>
-                <Card.Text>Quantity: 1</Card.Text>
-                <Button variant="danger" className="remove-button">Remove</Button>
-              </Card.Body>
-            </div>
+        {items.length > 0 ? (
+          <div>
+            {items.map((item) => (
+              <Card className="mb-3" key={item.id} style={{ borderColor: '#05652D', background: '#E3FCE9' }}>
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <Card.Img src={`product${item.id}.jpg`} alt={`Product ${item.id}`} />
+                  </div>
+                  <div className="col-md-8">
+                    <Card.Body>
+                      <Card.Title>{item.name}</Card.Title>
+                      <Card.Text>Price: {item.price}</Card.Text>
+                      <Card.Text>Quantity: {item.quantity}</Card.Text>
+                      <Button variant="danger" className="remove-button">
+                        Remove
+                      </Button>
+                      <Form.Check
+                        type="checkbox"
+                        className="position-absolute top-50 start-50 translate-middle"
+                        checked={selectedItems.includes(item)}
+                        onChange={(e) => handleCheckboxChange(e, item)}
+                        style={{ borderColor: '#05652D', width: '20px', height: '20px' }}
+                      />
+                    </Card.Body>
+                  </div>
+                </div>
+              </Card>
+            ))}
           </div>
-        </Card>
-
-        <Card className="mb-3">
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <Card.Img src="product2.jpg" alt="Product 2" />
-            </div>
-            <div className="col-md-8">
-              <Card.Body>
-                <Card.Title>Product 2</Card.Title>
-                <Card.Text>Price: Php 75.50</Card.Text>
-                <Card.Text>Quantity: 2</Card.Text>
-                <Button variant="danger" className="remove-button">Remove</Button>
-              </Card.Body>
-            </div>
+        ) : (
+          <div className="empty-cart text-center">
+            <p>Your cart is empty.</p>
           </div>
-        </Card>
+        )}
 
-        <div className="empty-cart text-center">
-          <p>Your cart is empty.</p>
+        <div className="detail-form mt-4">
+          <Form.Group controlId="voucherCode">
+            <Form.Label>ECO-lover Voucher</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Code"
+              value={voucherCode}
+              onChange={handleVoucherCodeChange}
+              style={{ borderColor: '#05652D' }}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="selectedItems">
+            <Form.Label>Item/s Selected</Form.Label>
+            <Form.Control type="text" value={selectedItems.length} readOnly />
+          </Form.Group>
+
+          <Form.Group controlId="discount">
+            <Form.Label>Discounts</Form.Label>
+            <Form.Control type="text" value={discount} readOnly />
+          </Form.Group>
+
+          <Form.Group controlId="amount">
+            <Form.Label>Amount</Form.Label>
+            <Form.Control type="text" value={calculateAmount()} readOnly />
+          </Form.Group>
         </div>
+
+        <Form.Group controlId="selectAll">
+          <Form.Check
+            type="checkbox"
+            label="Select All"
+            checked={selectAll}
+            onChange={handleSelectAllChange}
+            style={{ borderColor: '#05652D' }}
+          />
+        </Form.Group>
+
+        <Button
+              variant="primary"
+              className="mb-4"
+              style={{ borderColor: '#05652D', backgroundColor: '#05652D', width: '300px', margin: 'auto', display: 'block' }}
+            >
+          Check Out
+        </Button>
       </div>
       <Footer />
     </div>
