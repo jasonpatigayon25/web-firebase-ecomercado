b678bb6154cbabafe716e0f39d8f76c6fa0e2ee7
Jason Patigayon <jasonpatigayon25@gmail.com>
2023-07-26 23:15:56 +0800

    Project-Capstone-Admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // import './App.css'
 import Welcome from "./pages/Welcome"
-import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import ContactUs from "./pages/ContactUs"
@@ -8,11 +7,6 @@ import AboutUs from "./pages/AboutUs"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AdminDashboard from "./pages/AdminDashboard"
-import AdminSales from "./pages/AdminSales"
-import AdminDonations from "./pages/AdminDonations"
-import AdminSellerRequest from "./pages/AdminSellerRequest"
-import AdminUserFeedback from "./pages/AdminUserFeedback"
-import AdminUserStatistics from "./pages/AdminUserStatistics"
 import ProductViewer from "./pages/ProductViewer"
 import UserStatistics from "./pages/UserStatistics"
 import Donation from "./pages/Donation"
@@ -29,13 +23,7 @@ function App() {
           <Route path="/contact-us" element={<ContactUs/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
-          <Route path="/home" element={<Home/>}/>
           <Route path="/admin-dashboard" element={<AdminDashboard/>}/>
-          <Route path="/admin-sales" element={<AdminSales/>}/>
-          <Route path="/admin-donations" element={<AdminDonations/>}/>
-          <Route path="/admin-seller" element={<AdminSellerRequest/>}/>
-          <Route path="/admin-feedback" element={<AdminUserFeedback/>}/>
-          <Route path="/admin-users" element={<AdminUserStatistics/>}/>
           <Route path="/product-viewer" element={<ProductViewer/>}/>
           <Route path="/user-statistics" element={<UserStatistics/>}/>
           <Route path="/donations" element={<Donation/>}/>
