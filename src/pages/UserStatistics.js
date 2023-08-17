75669fd063ec22f2bad9824bd9404a5ad7fc12aa
Jason Patigayon <jasonpatigayon25@gmail.com>
2023-08-17 23:27:32 +0800

    Project-admin

--- src/pages/UserStatistics.js
+++ src/pages/UserStatistics.js
@@ -4,16 +4,15 @@ import "../css/Admin.css";
 import { FaUserCheck } from "react-icons/fa";
 import { db } from '../config/firebase';
 import { collection, getDocs, limit } from "firebase/firestore";
-
-const weeklyRegisteredUsers = 0;
+import { where, query } from "firebase/firestore";
 
 function UserStatistics() {
 
   const [totalUsers, setTotalUsers] = useState(0);
   const [recentFetchedUsers, setRecentFetchedUsers] = useState([]);
+  const [weeklyRegisteredUsers, setWeeklyRegisteredUsers] = useState(0);
 
   useEffect(() => {
-
     getDocs(collection(db, 'users'))
       .then(snapshot => {
         setTotalUsers(snapshot.size);
@@ -21,8 +20,32 @@ function UserStatistics() {
       .catch(err => {
         console.error("Error fetching products: ", err);
       });
+
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(today.getDate() - (dayOfWeek - 1));
+    startOfWeek.setHours(0, 0, 0, 0);
+
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6);
+    endOfWeek.setHours(23, 59, 59, 999);
+
+    const q = query(
+      collection(db, 'users'),
+      where('dateRegistered', '>=', startOfWeek),
+      where('dateRegistered', '<=', endOfWeek)
+    );
+
+    getDocs(q)
+      .then(snapshot => {
+        setWeeklyRegisteredUsers(snapshot.size);
+      })
+      .catch(err => {
+        console.error("Error fetching weekly registered users: ", err);
+      });
     
-      getDocs(collection(db, 'users'), limit(20))
+    getDocs(collection(db, 'users'), limit(20))
       .then(snapshot => {
           const fetchedUsers = snapshot.docs.map(doc => ({
               email: doc.data().email,
