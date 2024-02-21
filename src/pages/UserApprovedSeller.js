990f15230b2516ac77a2769f14452e11286969f4
Markymy <rocabertemarky09@gmail.com>
2024-02-21 20:01:45 +0800

    ECOMERCADO

--- src/pages/UserApprovedSeller.js
+++ src/pages/UserApprovedSeller.js
@@ -1,32 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import "../css/ActivityNavbar.css"; // Ensure this path is correct for your CSS
+import "../css/ActivityNavbar.css";
 import { db } from '../config/firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import Modal from 'react-modal'; // If you plan to use Modal, ensure it's implemented
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Ensure FontAwesome is correctly set up
-import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'; // Ensure you use these icons or remove if not needed
+import Modal from 'react-modal'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'; 
 
 function UserApprovedSeller() {
   const [products, setProducts] = useState([]);
 
-  // Fetch products with publicationStatus 'approved'
   useEffect(() => {
     const fetchApprovedProducts = async () => {
       const productsRef = collection(db, "products");
-      // Adjust the query to filter by publicationStatus 'approved'
       const q = query(productsRef, where("publicationStatus", "==", "approved"));
       const querySnapshot = await getDocs(q);
       const productList = querySnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data(),
-        // Convert createdAt if it's a Timestamp to a Date object, or handle as necessary
         createdAt: doc.data().createdAt?.toDate() ? doc.data().createdAt.toDate() : new Date(),
       }));
       setProducts(productList);
     };
 
     fetchApprovedProducts();
-  }, []); // Empty dependency array means this effect runs once on mount
+  }, []); 
 
   const renderProductApproved = () => (
     <table>
@@ -37,6 +34,7 @@ function UserApprovedSeller() {
           <th>Category</th>
           <th>Price</th>
           <th>Quantity</th>
+          <th>Seller</th>
           <th>Date Published</th>
         </tr>
       </thead>
@@ -49,6 +47,7 @@ function UserApprovedSeller() {
               <td>{product.category}</td>
               <td>₱{product.price}</td>
               <td>{product.quantity}</td>
+              <td>{product.seller_email}</td>
               <td>{product.createdAt.toLocaleDateString()}</td>
             </tr>
           ))
