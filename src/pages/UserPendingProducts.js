2e705f21117392707c83404b99f71948b2146e2c
Markymy <rocabertemarky09@gmail.com>
2024-04-26 22:10:39 +0800

    ECOMERCADO

--- src/pages/UserPendingProducts.js
+++ src/pages/UserPendingProducts.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import "../css/Products.css";
 import { db } from '../config/firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, doc, updateDoc } from 'firebase/firestore';
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -55,9 +55,35 @@ function UserPendingProducts() {
     );
   });
 
+  const handleApprove = async (productId) => {
+    try {
+      await updateDoc(doc(db, "products", productId), { publicationStatus: "approved" });
+      const updatedProducts = products.map(product => {
+        if (product.id === productId) {
+          return { ...product, publicationStatus: "approved" };
+        }
+        return product;
+      });
+      setProducts(updatedProducts);
+    } catch (error) {
+      console.error("Error updating document: ", error);
+    }
+  };
+
+  const handleDecline = async (productId) => {
+    try {
+      await updateDoc(doc(db, "products", productId), { publicationStatus: "declined" });
+
+      const updatedProducts = products.filter(product => product.id !== productId);
+      setProducts(updatedProducts);
+    } catch (error) {
+      console.error("Error updating document: ", error);
+    }
+  };
+
   const renderProductApproved = () => (
     <div className="product-list-container">
-      <h1 className="recent-products-title"><FaClipboardCheck style={{ marginRight: '8px', verticalAlign: 'middle' }} /> All Approved Products</h1>
+      <h1 className="recent-products-title"><FaClipboardCheck style={{ marginRight: '8px', verticalAlign: 'middle' }} /> All Pending Products</h1>
       <div className="search-bar-wrapper">
         <input
           type="text"
@@ -68,28 +94,33 @@ function UserPendingProducts() {
         />
       </div>
       {filteredProducts.length > 0 ? (
-        <ul className="product-list">
+        <ul  className="product-list">
           {filteredProducts.map(product => (
-            <li key={product.id} className="product-list-item" onClick={() => openModal(product)}>
+            <li onClick={() => openModal(product)} key={product.id} className="product-list-item">
               <img src={product.photo} alt={`${product.name} thumbnail`} className="product-list-photo" />
               <div className="product-info">
                 <div className="product-name">{product.name}</div>
                 <div className="product-detail">
-                  <span  className="product-price">₱{product.price}</span>
+                  <span className="product-price">₱{product.price}</span>
                   <span className="product-category">{product.category}</span>
-                  <span  className="product-qty">Qty: {product.quantity}</span>
-                  <span  className="product-seller">From: {product.seller_email}</span>
-                  <span className="product-published-date">Published At: {product.createdAt.toLocaleDateString()}</span>
+                  <span className="product-qty">Qty: {product.quantity}</span>
+                  <span className="product-seller">From: {product.seller_email}</span>
+                  <div className="product-actions">
+                  <button onClick={() => handleApprove(product.id)} className="approve-button">Approve</button>
+                  <button onClick={() => handleDecline(product.id)} className="decline-button">Decline</button>
                 </div>
+                </div>
+                
               </div>
             </li>
           ))}
         </ul>
       ) : (
-        <p>No approved products found.</p>
+        <p>No pending products found.</p>
       )}
     </div>
   );
+
   return (
     <div className="approved-products-container">
       {renderProductApproved()}
